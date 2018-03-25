6fc6b74ed4c7b5c0c80fb0908948328e7cb1f603
zhangyuan <zhangyuan1@niwodai.net>
2018-03-25 20:59:43 +0800

    ''

--- linqstreamdemo/src/testData.java
+++ linqstreamdemo/src/testData.java
@@ -2,13 +2,40 @@ import java.util.ArrayList;
 
 class testData
 {
+        public static ArrayList<Book> getBooks(int num)
+        {
+                ArrayList<Book> students = new ArrayList<>(num);
+
+                for(int i = 0; i < num; i++)
+                {
+                        students.add
+                                (
+                                        new Book
+                                                (
+                                                        "Book_" + String.valueOf(i),
+                                                        i % 80
+                                                )
+                                );
+                }
+
+                return students;
+        }
+
         public static ArrayList<Student> getStudents(int num)
         {
                 ArrayList<Student> students = new ArrayList<>(num);
 
                 for(int i = 0; i < num; i++)
                 {
-                        students.add(new Student("Student_" + String.valueOf(i), i % 80, i % 3 == 0 ? Sex.Boy : Sex.Girl));
+                        students.add
+                                (
+                                        new Student
+                                                (
+                                                        "Student_" + String.valueOf(i),
+                                                        i % 80, i % 3 == 0 ? Sex.Boy : Sex.Girl,
+                                                        getBooks(i%30)
+                                                )
+                                );
                 }
 
                 return students;
@@ -20,7 +47,14 @@ class testData
 
                 for(int i = 0; i < num; i++)
                 {
-                        teachers.add(new Teacher("Teacher_" + String.valueOf(i), getStudents(i % 300)));
+                        teachers.add
+                                (
+                                        new Teacher
+                                                (
+                                                        "Teacher_" + String.valueOf(i),
+                                                        getStudents(i % 300)
+                                                )
+                                );
                 }
 
                 return teachers;
@@ -33,17 +67,35 @@ enum Sex
         Girl
 }
 
+class  Book
+{
+        public String BookName;
+        public int Pages;
+
+        public Book(String bookName, int pages)
+        {
+                BookName = bookName;
+                Pages = pages;
+        }
+}
+
 class Student
 {
         public String Name;
         public int Age;
         public Sex Sex;
+        public ArrayList<Book> Books;
 
-        public Student(String name, int age, Sex sex)
+        public Student()
+        {
+        }
+
+        public Student(String name, int age, Sex sex, ArrayList<Book> books)
         {
                 Name = name;
                 Age = age;
                 Sex = sex;
+                Books = books;
         }
 }
 
@@ -52,6 +104,10 @@ class Teacher
         public String Name;
 
         public ArrayList<Student> Students;
+
+        public Teacher()
+        {
+        }
 
         public Teacher(String name, ArrayList<Student> students)
         {
